ffed12b77ba92f5f663bb1bb2aab1df64a4f7b21
Victor <medina.levic24@triana.salesianos.edu>
2023-12-14 13:40:54 +0100

    ns

--- EjerciciosBoletin/src/ejercicio03/Principal.java
+++ EjerciciosBoletin/src/ejercicio03/Principal.java
@@ -10,13 +10,13 @@ public class Principal {
 	int[] my_array = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
 		        
 	// Insert an element in 3rd position of the array (index->2, value->5)
-		        
+			        
 	int Index_position = 2;
 	int newValue    = 5;
 		        
 	System.out.println("Original Array : "+Arrays.toString(my_array));
 	
-	for(int i=my_array.length; i >= my_array.length; i++){
+	for(int i= 0; i >= my_array.length; i++){
 		my_array[i] = my_array[i-1];
 	}
 	my_array[Index_position] = newValue;
